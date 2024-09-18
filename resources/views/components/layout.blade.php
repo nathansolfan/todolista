531efa7d49eb71fab6a2aaa955720b46c5407dda
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-09-18 22:39:29 +0100

    updates

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> {{ $title ?? 'My T O  D O Application'}} </title>
+    @vite('resources/css/app.css')
+
 </head>
 <body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <!-- <a href="/create">Click me</a> -->
     <a href=" {{ route('tasks.create')}} ">Create</a>
     {{-- <a href=" {{ route('tasks.show')}} ">Show me</a> --}}
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
     @foreach ( $tasks as $task )
     <p>{{$task->title}}</p>
     <p>{{$task->description}}</p>
     <a href=" {{ route('tasks.show', $task->id)}} ">Show task</a>        
     @endforeach
-    
+
 </x-layout>
