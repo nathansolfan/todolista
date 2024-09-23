8cbfec3bf38673b4bb6a956c4645cd5cf5e5cacb
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-09-23 21:24:29 +0100

    update edit method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,8 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        return view('users.edit', ['user' => $user]);
     }
 
     /**
