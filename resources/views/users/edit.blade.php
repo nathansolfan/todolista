59839eda33442f948c047ac426de92dc89ec0ccd
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-09-23 21:30:43 +0100

    edit blade template updated with old value

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,16 +2,17 @@
     <form action=" {{route('tasks.update')}} " method="POST">
         <div>
             <label for="">Name</label>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" value=" {{old('name', $user->name)}} ">
         </div>
         <div>
             <label for="">Email</label>
-            <input type="text" name="email" id="email">
+            <input type="text" name="email" id="email" value=" {{old('email', $user->email)}} ">
         </div>
         <div>
             <label for="">Password</label>
             <input type="text" name="password" id="password">
         </div>
+        <button type="submit" >Update me</button>
 
     </form>
 </section>
