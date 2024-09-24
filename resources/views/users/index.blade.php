0ba2161ed4b721660f19970889ff4fbbe49eae24
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-09-24 20:04:04 +0100

    added styling into users.index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,13 +1,16 @@
 <x-layout>
-    <a href=" {{ route('users.create')}} ">Create User</a>
-    
-
-    @foreach ( $users as $user)
-    <div>
-        <p> {{ $user->name}} </p>
-        <p> {{ $user->email}} </p>
+    <div class="flex justify-between items-center mb-4">
+        <a href=" {{ route('users.create')}}" class="bg-blue-500 text-white px-4 py-2 rounded">Create User</a>
     </div>
-    <a href=" {{ route('users.show', $user->id)}} "> Show me </a>        
-    @endforeach
+
+    <div class="grid gap-6">
+        @foreach ($users as $user)
+        <div class="bg-gray-100 p-6 rounded-lg shadow-lg">
+            <p class="text-xl font-semibold">{{ $user->name }}</p>
+            <p class="text-gray-600">{{ $user->email }}</p>
+            <a href="{{ route('users.show', $user->id) }}" class="text-blue-500 hover:underline mt-2 block">Show me</a>
+        </div>
+        @endforeach
+    </div>
     
 </x-layout>
